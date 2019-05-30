937ef809035a10aa8fc00e0e601930a00656a1da
Peter <cirediatpl@gmail.com>
2019-05-30 14:37:47 -0400

    blah blah

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,4 +1,36 @@
 class Attendee
+
+    attr_reader :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def events
+        all_tickets = Ticket.all.select do |ticket|
+            ticket.attendee == self
+        end
+        all_tickets.map do |ticket|
+            ticket.event
+        end
+    end
+
+    def money_spent
+        sum = events.map do |event|
+            event.ticket_price
+        end.inject do |sum, element|
+            sum + element
+        end
+    end
+
 end
 
 # Attendee.all
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,42 @@
 class Event
+
+    attr_reader :name
+    attr_accessor :total_cost, :ticket_price
+
+    @@all = []
+
+    def initialize(name, total_cost, ticket_price)
+        @name = name
+        @total_cost = total_cost
+        @ticket_price = ticket_price
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def sell_to_break_even
+        remaining_cost = @total_cost - (@ticket_price * Ticket.all.count)
+        remaining_cost / @ticket_price
+    end
+
+    def attendees
+        Ticket.all.select do |ticket|
+            ticket.event == self
+        end.map do |ticket|
+            ticket.attendee
+        end
+    end
+
+    def average_age
+        sum = attendees.map do |attendee|
+            attendee.age
+        end.inject do |sum, element|
+            sum + element
+        end.to_f
+        sum / attendees.size
+    end
 end
 
 # Event.all
